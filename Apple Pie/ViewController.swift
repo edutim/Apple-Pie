53a9bf4315e05e4390b22d369d510e70b41edc6c
Tim Hart <tim@9red7.com>
2022-01-10 12:11:59 -0500

    updates

--- Apple Pie/ViewController.swift
+++ Apple Pie/ViewController.swift
@@ -18,8 +18,16 @@ class ViewController: UIViewController {
   var listOfWords = ["buccaneer", "swift", "incandescent", "bug", "program"]
   
   var incorrectMovesAllowed = 7
-  var totalWins = 0
-  var totalLosses = 0
+  var totalWins = 0 {
+    didSet {
+      newRound()
+    }
+  }
+  var totalLosses = 0 {
+    didSet {
+      newRound()
+    }
+  }
   
   var currentGame: Game!
   
@@ -29,9 +37,14 @@ class ViewController: UIViewController {
     }
 
   func newRound() {
-    let newWord = listOfWords.removeFirst()
-    currentGame = Game(word: newWord, incorrectMovesRemaining: incorrectMovesAllowed, guessedLetters: [])
-    updateUI()
+    if !listOfWords.isEmpty {
+      let newWord = listOfWords.removeFirst()
+      currentGame = Game(word: newWord, incorrectMovesRemaining: incorrectMovesAllowed, guessedLetters: [])
+      enableLetterButtons(true)
+      updateUI()
+    } else {
+      enableLetterButtons(false)
+    }
   }
   
   func updateUI() {
@@ -50,7 +63,24 @@ class ViewController: UIViewController {
     let letterString = sender.configuration!.title!
     let letter = Character(letterString.lowercased())
     currentGame.playerGuessed(letter: letter)
-    updateUI()
+    updateGameState()
   }
+  
+  func enableLetterButtons(_ enable: Bool) {
+    for button in letterButtons {
+      button.isEnabled = enable
+    }
+  }
+  
+  func updateGameState() {
+    if currentGame.incorrectMovesRemaining == 0 {
+      totalLosses += 1
+    } else if currentGame.word == currentGame.formattedWord {
+      totalWins += 1
+    } else {
+      updateUI()
+    }
+  }
+  
 }
 
